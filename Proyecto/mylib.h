a91013cd292d4ee1054d754b9a4221665d19def3
josevicenteguillenmainez <josevicente.guillen.mainez@alumnos.upm.es>
2019-04-24 10:49:53 +0200

    Create mylib.h

---
+++ Proyecto/mylib.h
@@ -0,0 +1,8 @@
+#define NUMT1 8     //numero temas de la primera parte
+#include <windows.h>
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+void escribeTexto(char vector[]); //funcion para que las letras vayan con retraso
+void temasrand(int aleatorio[NUMT1]); //generador de vector aleatorio
