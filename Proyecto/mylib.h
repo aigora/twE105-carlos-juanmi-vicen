45a5f60dd71b82d5cc22ecf5e8df5c38b0f256ee
jmcabornero <jm.cabornero@alumnos.upm.es>
2019-04-26 13:43:36 +0200

    Update mylib.h

--- Proyecto/mylib.h
+++ Proyecto/mylib.h
@@ -6,3 +6,4 @@
 #include <string.h>
 void escribeTexto(char vector[]); //funcion para que las letras vayan con retraso
 void temasrand(int aleatorio[NUMT1]); //generador de vector aleatorio
+void abrirficherospreguntas(void); //abre todos los ficheros de las preguntas
