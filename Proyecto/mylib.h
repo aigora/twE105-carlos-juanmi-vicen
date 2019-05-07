9aa39d0abee4d9da9a8a80ab05933a09ce5d35e0
josevicenteguillenmainez <josevicente.guillen.mainez@alumnos.upm.es>
2019-05-07 23:50:22 +0200

    Update mylib.h

--- Proyecto/mylib.h
+++ Proyecto/mylib.h
@@ -5,7 +5,6 @@
 #define NUMR1 4     //numero respuestas de la primera parte
 #define NUMR2 3 	//numero respuestas de la segunda parte
 #define NUMR3 2 	//numero respuestas de la tercera parte
-#define NUMP 2 //numero preguntas en cada tema
 #include <windows.h>
 #include <stdio.h>
 #include <math.h>
@@ -13,5 +12,4 @@
 #include <string.h>
 void escribeTexto(char vector[]); //funcion para que las letras vayan con retraso
 void vectorrand8(int aleatorio[NUMT1]); //generador de vector aleatorio de 8
-void abrirficherospreguntas(void); //abre todos los ficheros de las preguntas
 void vectorrand2(int aleatorio[NUMP]); //generador de vector aleatorio de 2
